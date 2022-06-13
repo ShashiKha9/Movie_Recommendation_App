16f801aa9f8edddb25dd091a3f321b9d496de4c7
ShashiKha9 <87931813+ShashiKha9@users.noreply.github.com>
2022-06-13 12:07:27 +0530

    movies screen done

--- lib/screens/movies.dart
+++ lib/screens/movies.dart
@@ -13,7 +13,6 @@ class _MovieScreenState extends State<MovieScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.black,
         actions: [
@@ -29,7 +28,7 @@ class _MovieScreenState extends State<MovieScreen> {
         ],
       ),
       body: SafeArea(
-          child: Padding(padding: EdgeInsets.all(10.0),
+          child: Padding(padding: EdgeInsets.all(8.0),
           child: SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -46,9 +45,103 @@ class _MovieScreenState extends State<MovieScreen> {
                                 + widget.movie["backdrop_path"]),
                             fit:BoxFit.cover )
                       ),
-                    )
-
+                    ), 
+                    IconButton(onPressed:()=> print("Play"),
+                        icon: Icon(Icons.play_circle_outline,
+                          color: Colors.white,size: 40,)),
                   ],
+                ),
+                const SizedBox(
+                  height: 7,),
+                Text(widget.movie["title"],
+                  style: TextStyle(fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                  fontSize: 18),),
+
+                Text(widget.movie["release_date"],
+                  style: TextStyle(
+                      color: Colors.grey[300],
+                      fontSize: 15),),
+                Padding(padding: EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    height: 40,
+                    child: ElevatedButton(onPressed:(){},
+                        child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.play_arrow,color: Colors.black,),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text("Play",style: TextStyle(color: Colors.black),),
+                          ],
+                        ),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.white)
+                    ),),
+
+                  ),
+                ),
+                Padding(padding: EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    height: 40,
+                    child: ElevatedButton(onPressed:(){},
+                      child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.download,color: Colors.white,),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text("Download",style: TextStyle(color: Colors.white),),
+                        ],
+                      ),
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(Colors.grey[900])
+                      ),),
+
+                  ),
+                ),
+                Text(widget.movie["overview"],
+                  style: const TextStyle(color: Colors.white70,
+                      fontSize: 15,
+                  ),
+                  maxLines: 6,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  width: 120,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.red
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: const [
+                      Icon(Icons.thumb_up_sharp,
+                        color: Colors.white,
+                        size: 20,),
+                      Text("Most Liked",
+                        style: TextStyle(color: Colors.white,
+                            fontWeight: FontWeight.bold),)
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      buttons(Icons.add, "My List"),
+                      buttons(Icons.thumb_up_sharp, "Rate"),
+                      buttons(Icons.share, "Share")
+
+                    ],
+                  ),
                 )
 
               ],
@@ -56,4 +149,13 @@ class _MovieScreenState extends State<MovieScreen> {
           ),)),
     );
   }
+ Widget  buttons(IconData icons,String name){
+    return Column(
+      children: [
+        IconButton(onPressed: (){},
+            icon: Icon(icons,color: Colors.white,)),
+        Text(name,style: const TextStyle(color: Colors.white),)
+      ],
+    );
+ }
 }
